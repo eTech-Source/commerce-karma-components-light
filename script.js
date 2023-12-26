64f316b776d3b52ad2dff18c3052a5117144f45f
eTech-Source <goetch777@gmail.com>
2023-12-26 13:14:43 -0800

    Finsh API key functions

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
-const commerceKarmaUrl = "https://commerce-karma.vercel.app";
-
 // Must be defined first
+const commerceKarmaUrl = "http://localhost:3000";
 
 const injectSignIn = () => {
   const signInContainer = document.getElementById("CK-signin");
@@ -34,31 +33,23 @@ const createStarElement = (src, alt) =>
 
 const createStars = (count) => {
   let starsHtml = "";
-  
+
   const fullStars = Math.floor(count);
   const halfStars = Math.round((count % 1) * 10) / 10;
   const emptyStars = 5 - fullStars - (halfStars > 0 ? 1 : 0);
 
   for (let i = 0; i < fullStars; i++) {
-    starsHtml += createStarElement(
-      "/assets/full-star.png",
-      "Rating star"
-    );
+    starsHtml += createStarElement("/assets/full-star.png", "Rating star");
   }
 
   if (halfStars > 0) {
-    starsHtml += createStarElement(
-      "/assets/half-star.png",
-      "Rating half star"
-    );
+    starsHtml += createStarElement("/assets/half-star.png", "Rating half star");
   }
-  
+
   for (let i = 0; i < emptyStars; i++) {
-    starsHtml += createStarElement(
-      "/assets/empty-star.png",
-    );
+    starsHtml += createStarElement("/assets/empty-star.png");
   }
-  
+
   return starsHtml;
 };
 
@@ -83,12 +74,14 @@ const get = async (id, filters, url) => {
   // Required to complete any requests other than OPTIONS
 
   const authorization = readCookie("CK-jwt");
+  const apiKey = readCookie("CK-api-key")
 
   const rawResponse = await fetch(url, {
     headers: {
       id: id,
       filters: encodedFilters,
       Authorization: authorization,
+      "Api-Key": apiKey,
     },
   });
 
@@ -134,14 +127,21 @@ const injectUser = async (link, filters) => {
     injectHtml += `        
           <div class="CK-user">
              <a href="" class="CK-user-link">
-                <h1 class="CK-user-name">${users[i].name} <b class="CK-star-number">(${users[i].customerRating})</b></h1>
-                <div id="CK-user-stars-${users[i]._id}" class="CK-stars">${createStars (users[i].customerRating)}</div>
+                <h1 class="CK-user-name">${
+                  users[i].name
+                } <b class="CK-star-number">(${
+      users[i].customerRating
+    })</b></h1>
+                <div id="CK-user-stars-${
+                  users[i]._id
+                }" class="CK-stars">${createStars(
+      users[i].customerRating
+    )}</div>
               </a>
           </div>`;
   }
 
   usersContainer.innerHTML = injectHtml;
-
 };
 
 // Authentication: DO NOT MODIFY
@@ -159,10 +159,30 @@ const checkAuth = async (users, reviews) => {
   }
 };
 
+const checkApiKey = async (apiKey) => {
+  try {
+    const rawResponse = await fetch(`${commerceKarmaUrl}/api/keys`, {
+      headers: { Authorization: apiKey },
+    });
+    const response = await rawResponse.json();
+    if (rawResponse.status >= 400) {
+      throw new Error(response.message);
+    }
+    return {apiKey, response};
+  } catch (error) {
+    return error;
+  }
+};
+
 document.addEventListener("DOMContentLoaded", async () => {
+  const key = await checkApiKey(
+    "eyJhbGciOiJIUzI1NiJ9.eyJhY2Nlc3NUb2tlbiI6IisuSFxcQWp1fD5ffS9zOHB-emQrTWxCc0xwZXMzdiQ0Q3g8bTtCQFtle0w4XCI7ZjBTMm9GXCIvcitDRmdXXT5WVjgiLCJ1c2VyIjoiNjU3NGZiZjM0YzFjMWFjYjIyNjI0ZTRiIiwiaWF0IjoxNzAzNTQ0MTA3LCJpc3MiOiJlVGVjaCAoQ29tbWVyY2UgS2FybWEpIiwiYXVkIjoiYmlkcy5yZXNwb25zaWJpZC5jb20ifQ.sLdK07TXdshkTXDJdLQb-YRvx9ZZfE1EPbjbVdcejDY"
+  );
   const users = await injectUser();
-  await checkAuth(users,);
+  if (!(key.response instanceof Error)) {
+    document.cookie = `CK-api-key=${key.apiKey}; SameSite=None; Secure=true; Expires= 1 Jan 3000 00:00:01 GMT`
+  }
 });
 
 // Exports
-export {injectSignIn, checkAuth}
+export { injectSignIn, checkAuth, injectUser };
